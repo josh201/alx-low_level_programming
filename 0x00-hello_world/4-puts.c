b1ed603f0ee44ea7eb0728ec263ce707479a1419
joshua <Joshuaoajibade@gmail.com>
2022-09-10 02:30:28 +0100

    Puts_&_execute

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
   *main -Entry point
-  *puts: this print the value entered
+  *
   *Return: Always 0 (Success)
   */
 
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
