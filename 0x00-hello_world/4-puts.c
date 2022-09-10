2e3a0b5696598084e024e264e25ee0cea7a3d4be
joshua <Joshuaoajibade@gmail.com>
2022-09-10 02:15:09 +0100

    Puts_&_execute

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,14 @@
 
 #include<stdio.h>
 
+/**
+  *main -Entry point
+  *puts: this print the value entered
+  *Return: Always 0 (Success)
+  */
+
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0)
 }
