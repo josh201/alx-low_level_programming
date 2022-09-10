7feaae3cb312f827df17081a7964d13459a6e212
joshua <Joshuaoajibade@gmail.com>
2022-09-10 03:34:56 +0100

    UNIX basically simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  *main -Entry point
+  *
+  *Description: ' the useful piece of art'
+  *
+  *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
+
