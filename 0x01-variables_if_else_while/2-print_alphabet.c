6a87b31792d515663d3b54aea7750d4fa111301a
joshua <Joshuaoajibade@gmail.com>
2022-09-11 11:53:08 +0100

    putchar_2-print_alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	/* declare variable */
+	char c;
+	
+	/* for loop statement */
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar('\n');
+
+return (0);
+}
+ 
