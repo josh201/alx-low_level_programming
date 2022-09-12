f3f559fb4f135cf0b4cb8f926eb0a78ef6dce54a
Joshua <Joshuaoajibade@gmail.com>
2022-09-12 16:27:28 -0700

    102-print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,22 +9,20 @@ int main(void)
 {
 	int a;
 	int b;
-	
-	for (a = '0'; a <= '9'; a++)
-	{
-		for (b = '1'; b <= '9'; b++)
-		{
-			putchar(a);
-			putchar(a);
-			putchar(' ');
-			putchar(a);
-			putchar(b);
 
-			if (a <= '9')
-			{
-				putchar(',');
-				putchar(' ');
-			}
+	for (a = 0; a <= 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
