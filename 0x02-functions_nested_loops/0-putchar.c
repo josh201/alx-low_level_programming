103c30fed43ba5a45799cac07815613ac05a060b
joshua <Joshuaoajibade@gmail.com>
2022-09-13 23:03:49 +0100

    Print_char.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(p[i]);
+	}
+	return (0);
+}
