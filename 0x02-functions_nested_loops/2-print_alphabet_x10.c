366f92a4ca0ceb3f6f4db0faa3175552cdcc36c1
joshua <Joshuaoajibade@gmail.com>
2022-09-14 03:14:30 +0100

    push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,8 +5,16 @@
   *
   * Return: Always 0.
   */
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	int i, a;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 97; a <= 122; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
 }
