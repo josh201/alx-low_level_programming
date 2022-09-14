535c901a49915bca949aafd118444faf53ee7d79
joshua <Joshuaoajibade@gmail.com>
2022-09-14 03:17:52 +0100

    push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-  * main - check the code.
-  *
-  * Return: Always 0.
+ * print_alphabet_x10 - prints the alphabet 10 times.
+ *
+ * Return: Always 0.
   */
 void print_alphabet_x10(void)
 {
