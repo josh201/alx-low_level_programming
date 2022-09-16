54f1a324d5499c3d08b8f21b9159f51ea1f847ae
joshua <Joshuaoajibade@gmail.com>
2022-09-16 01:59:30 +0100

    2-0x04

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_line - draw a straight line using charaster -
+  * @n: the number of - characters to be printed.
+  */
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
