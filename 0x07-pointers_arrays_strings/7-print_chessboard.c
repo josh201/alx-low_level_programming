11a8bafc11aeda374239e460f1657241336e70c6
joshua <Joshuaoajibade@gmail.com>
2022-09-28 12:53:06 +0100

    2-0x07

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * print_chessboard - a function that 
-  * @a: an array input to print
-  * Return: Nothing
-  */
+ * print_chessboard - a function that prints a chessboard
+ * @a: an array input to print
+ * Return: Nothing
+ */
 
 void print_chessboard(char (*a)[8])
 {
