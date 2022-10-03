120833e084bd066b90041760db72bca4beb77556
joshua <Joshuaoajibade@gmail.com>
2022-10-03 23:55:36 +0100

    0x0B-malloc_free

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- kevin mitnick");
+	_puts("\"At the end of the day,
+			my goal was to be the best hacker\"\n\t- kevin mitnick");
 	return (0);
 }
