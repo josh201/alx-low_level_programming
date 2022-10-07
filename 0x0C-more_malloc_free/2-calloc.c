042f37c26ec4465a760ea082997756371a438683
joshua <Joshuaoajibade@gmail.com>
2022-10-07 05:31:32 +0100

    0x0C-more_malloc_free1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	space = malloc(cmemb * size);
+	space = malloc(nmemb * size);
 
 	if (space == 0)
 		return (NULL);
